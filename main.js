8bcbc6bc549a0b9ed3644286c3719e7c11db0b9a
home <woongdam777@gmail.com>
2023-11-26 04:14:44 +0900

    typing_분석

--- main.js
+++ main.js
@@ -40,11 +40,9 @@ resetFirstScreen.addEventListener('click',() => {
 });
 
 document.addEventListener('DOMContentLoaded',function(event){
-    // array with texts to type in typewriter
-    var dataText = [ "안녕하세요.", "개발자 권재웅입니다.", "타이핑효과", "적용중입니다."];
+
+    let textArr = [ "안녕하세요.", "타이핑효과", "적용중입니다."];
     
-    // type one text in the typwriter
-    // keeps calling itself until the text is finished
     function typeWriter(text, i, fnCallback) {
       // chekc if text isn't finished yet
       if (i < (text.length)) {
@@ -59,20 +57,20 @@ document.addEventListener('DOMContentLoaded',function(event){
       // text finished, call callback if there is a callback function
       else if (typeof fnCallback == 'function') {
         // call callback after timeout
-        setTimeout(fnCallback, 700);
+        setTimeout(fnCallback, 800);
       }
     }
     // start a typewriter animation for a text in the dataText array
      function StartTextAnimation(i) {
-       if (typeof dataText[i] == 'undefined'){
+       if (typeof textArr[i] == 'undefined'){
           setTimeout(function() {
             StartTextAnimation(0);
           }, 20000);
        }
        // check if dataText[i] exists
-      if (i < dataText[i].length) {
+      if (i < textArr[i].length) {
         // text exists! start typewriter animation
-       typeWriter(dataText[i], 0, function(){
+       typeWriter(textArr[i], 0, function(){
          // after callback (and whole text has been animated), start next text
          StartTextAnimation(i + 1);
        });
